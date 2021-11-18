e9bff715bbdc70d869432f5de04a95b1a81a21a2
Jim <jim.huang@ecareme.com>
2021-11-18 11:31:50 +0800

    add : add file picker page

---
+++ Library-iOS/Extension/Bundle.swift
@@ -0,0 +1,21 @@
+//
+//  Bundle.swift
+//  Library-iOS
+//
+//  Created by jim on 2021/10/1.
+//
+
+import Foundation
+import UIKit
+
+extension Bundle {
+    public var icon: UIImage? {
+        if let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+           let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+           let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+           let lastIcon = iconFiles.last {
+            return UIImage(named: lastIcon)
+        }
+        return nil
+    }
+}
--- Library-iOS/View/FilePickerViewController.swift
+++ Library-iOS/View/FilePickerViewController.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 import UIKit
+import LinkPresentation
 
 class FilePickerViewController: BaseViewController {
     
+    private var documentInteraction: UIDocumentInteractionController? = nil
     private var documentPicker: UIDocumentPickerViewController? = nil
-    
+    private var sharePicker: UIActivityViewController? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -31,6 +34,31 @@ class FilePickerViewController: BaseViewController {
         self.documentPicker!.delegate = self
         self.present(self.documentPicker!, animated: true, completion: nil)
     }
+    
+    /**
+     * UIActivityViewController：Open Share Picker, share data like string images...
+     */
+    @IBAction func openSharePickerFunction(_ sender: Any) {
+        if self.sharePicker == nil {
+            self.sharePicker = UIActivityViewController(activityItems: [self], applicationActivities: nil)
+        }
+        
+        self.present(self.sharePicker!, animated: true, completion: nil)
+    }
+    
+    /**
+     * UIDocumentInteractionController：Open  File Share View, share document or files like PDF
+     */
+    @IBAction func openDocumentInteractionFunction(_ sender: Any) {
+        self.documentInteraction = UIDocumentInteractionController()
+        self.documentInteraction!.url = NSURL.fileURL(withPath: Bundle.main.path(forResource: "Info", ofType: "plist")!)
+        self.documentInteraction!.delegate = self
+        
+        let isOpen = self.documentInteraction!.presentOpenInMenu(from: self.view.bounds, in: self.view, animated: true)
+        if !isOpen {
+            print("open document error")
+        }
+    }
 }
 
 extension FilePickerViewController: UIDocumentPickerDelegate {
@@ -44,4 +72,51 @@ extension FilePickerViewController: UIDocumentPickerDelegate {
             }
         }
     }
+}
+
+extension FilePickerViewController: UIActivityItemSource {
+    
+    /**
+     * 判斷分享目標的App 類型給予對應的值
+     */
+    func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        if activityType == .mail || activityType == .message || activityType == UIActivity.ActivityType(rawValue: "jp.naver.line.Share") {
+            return "Message"
+        }
+        
+        return "String"
+    }
+    
+    /**
+     * 返回的資料類型
+     */
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return "String"
+    }
+    
+    /**
+     * Email 的主旨
+     */
+    func activityViewController(_ activityViewController: UIActivityViewController, subjectForActivityType activityType: UIActivity.ActivityType?) -> String {
+        return "Email Title"
+    }
+    
+    /**
+     * 設定Share Picker 時顯示的Title、SubTitle、Icon
+     */
+    func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
+        let metaData = LPLinkMetadata()
+        metaData.title = "Share Title"
+        metaData.originalURL = NSURL.fileURL(withPath: "Share Subtitle")
+        
+        if let icon = Bundle.main.icon {
+            metaData.imageProvider = NSItemProvider(object: icon)
+            metaData.iconProvider = NSItemProvider(object: icon)
+        }
+        
+        return metaData
+    }
+}
+
+extension FilePickerViewController: UIDocumentInteractionControllerDelegate {
 }

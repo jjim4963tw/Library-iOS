157fb87b4b3e83c99dc23da131139b81f5bb1de2
Jim-Huang <jim.huang@asuscloud.com>
2022-03-22 17:30:07 +0800

    add : add api connection function and string extension

---
+++ Library-iOS/Extension/Array.swift
@@ -0,0 +1,12 @@
+//
+//  Array.swift
+//  Library-iOS
+//
+//  Created by 黃佳俊 on 2022/3/22.
+//
+
+import Foundation
+
+extension Array {
+    
+}
---
+++ Library-iOS/Utility/EncryptionUtility.swift
@@ -0,0 +1,8 @@
+//
+//  EncryptionUtility.swift
+//  Library-iOS
+//
+//  Created by 黃佳俊 on 2022/3/22.
+//
+
+import Foundation

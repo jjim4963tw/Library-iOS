f1f56bd442b1b30a872f9a92aa817a3c301baea3
Jim-Huang <jim.huang@asuscloud.com>
2022-03-30 17:15:16 +0800

    refactor : refactor project to swiftUI

---
+++ Library-iOS/Library_iOS.swift
@@ -0,0 +1,19 @@
+//
+//  Library_iOSApp.swift
+//  Library-iOS
+//
+//  Created by 黃佳俊 on 2022/3/30.
+//
+
+import SwiftUI
+
+@main
+struct Library_iOS: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                IndexView()
+            }
+        }
+    }
+}
---
+++ Library-iOS/View/IndexView.swift
@@ -0,0 +1,67 @@
+//
+//  IndexView.swift
+//  Library-iOS
+//
+//  Created by 黃佳俊 on 2022/3/30.
+//
+
+import SwiftUI
+
+struct IndexView: View {
+    @StateObject private var viewModel: IndexViewModel
+    
+    init() {
+        _viewModel = StateObject(wrappedValue: IndexViewModel())
+    }
+    
+    var body: some View {
+        List(self.viewModel.itemList, id: \.self) { content in
+            
+            switch content {
+            case "ImageLoader":
+                NavigationLink(destination: Text("Show Detail Here...")) {
+                    Text(content)
+                }
+            case "FilePicker":
+                NavigationLink(destination: Text("Show Detail Here...")) {
+                    Text(content)
+                }
+            case "SwitchGridorList":
+                NavigationLink(destination: Text("Show Detail Here...")) {
+                    Text(content)
+                }
+            case "FaceID":
+                NavigationLink(destination: FaceIDViewController()) {
+                    Text(content)
+                }
+            default:
+                NavigationLink(destination: Text("Show Detail Here...")) {
+                    Text(content)
+                }
+            }
+        }
+        .listStyle(.grouped)
+        .navigationTitle("Library-iOS")
+    }
+}
+
+struct IndexView_Previews: PreviewProvider {
+    static var previews: some View {
+        IndexView()
+    }
+}
+
+extension IndexView {
+    @MainActor class IndexViewModel: ObservableObject {
+        @Published var itemList: Array<String> = [String]()
+
+        init() {
+            self.itemList = [
+                "ImageLoader",
+                "FilePicker",
+                "SwitchGridorList",
+                "FaceID",
+            ]
+        }
+    }
+}
